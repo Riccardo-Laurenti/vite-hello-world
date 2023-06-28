9d161af88c3c770d4ebc123d8a04ecc9022035be
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-06-28 18:22:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/AppTitle.vue'
+</script>
+
+<template>
+  
+  <HelloWorld msg="Ciao" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="title">{{titolo}}</h1>
+</template>
+
+<script>
+  export default {
+      data() {
+          return{
+              titolo: 'La mia prima app con Vite!'
+          }
+      }
+  }
+</script>
+
+<style>
+  .title{
+      color: rgb(80, 81, 167);
+  }
+</style>
